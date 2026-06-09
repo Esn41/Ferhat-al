db43c92956eb4ad80ca7248848f852c20e3fa4f7
Esn41 <151942028+Esn41@users.noreply.github.com>
2026-06-09 12:55:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-const CACHE='ferhat-ai-v1';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => caches.delete(key)));
+    })
+  );
+  self.clients.claim();
+});
